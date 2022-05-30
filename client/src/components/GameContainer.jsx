4c22a872e9444cadf71d2c85021132fefa5d3c72
Manish Bhandari <bhmanish73@gmail.com>
2022-05-30 15:11:14 -0500

    added extra options for settings

--- client/src/components/GameContainer.jsx
+++ client/src/components/GameContainer.jsx
@@ -8,33 +8,37 @@ const options = {
   words: [10, 25, 50, 100],
   quotes: ["short", "medium", "long"],
 };
+const extras = ["punctuations", "numbers"];
 
 export default function GameContainer() {
   const [gamemode, setGamemode] = useState("time");
   const [option, setOption] = useState(undefined);
+  const [extraOptions, setExtraOptions] = useState([]);
+
+  const [extraEnabled, setExtraEnabled] = useState(true);
 
   useEffect(() => {}, [option]);
   useEffect(() => {
     setOption(options[gamemode][0]);
+    setExtraOptions([]);
+    setExtraEnabled(gamemode != "quotes");
   }, [gamemode]);
 
   if (!option) return;
+
+  const updateExtras = (extra) => {
+    // if already exists, remove it
+    if (extraOptions.includes(extra)) {
+      setExtraOptions(extraOptions.filter((ext) => ext !== extra));
+    } else {
+      setExtraOptions([...extraOptions, extra]);
+    }
+  };
 
   return (
     <div className="game">
       <div className="game_container">
         <div className="settings">
-          <div className="gamemodes setting">
-            {gamemodes.map((mode, index) => (
-              <div
-                className={`gamemode ${gamemode == mode ? "active" : ""}`}
-                key={index}
-                onClick={() => setGamemode(mode)}
-              >
-                {mode}
-              </div>
-            ))}
-          </div>
           <div className="options setting">
             {options[gamemode].map((opt, index) => (
               <div
@@ -45,6 +49,31 @@ export default function GameContainer() {
                 {opt}
               </div>
             ))}
+          </div>
+          <div className="rightside">
+            <div className="extras setting">
+              {extras.map((item) => (
+                <div
+                  className={`addition ${
+                    extraOptions.includes(item) ? "active" : ""
+                  } ${!extraEnabled ? "disabled" : ""}`}
+                  onClick={() => updateExtras(item)}
+                >
+                  {item}
+                </div>
+              ))}
+            </div>
+            <div className="gamemodes setting">
+              {gamemodes.map((mode, index) => (
+                <div
+                  className={`gamemode ${gamemode == mode ? "active" : ""}`}
+                  key={index}
+                  onClick={() => setGamemode(mode)}
+                >
+                  {mode}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         <Game />
